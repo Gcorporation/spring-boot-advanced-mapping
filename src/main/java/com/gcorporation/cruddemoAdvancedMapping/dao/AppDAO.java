a7b390004881bfe9dc6f1f9ec19bc15862992184
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-16 13:39:38 +0530

    Develop the main app

---
+++ src/main/java/com/gcorporation/cruddemoAdvancedMapping/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.gcorporation.cruddemoAdvancedMapping.dao;
+
+import com.gcorporation.cruddemoAdvancedMapping.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+}
