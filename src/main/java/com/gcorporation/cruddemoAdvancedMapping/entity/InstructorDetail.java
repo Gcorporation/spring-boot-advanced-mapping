eae63679b27ad4ff495cf9df5dcc06957f51205e
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-19 10:43:02 +0530

    Bi-Directional code Part 1

--- src/main/java/com/gcorporation/cruddemoAdvancedMapping/entity/InstructorDetail.java
+++ src/main/java/com/gcorporation/cruddemoAdvancedMapping/entity/InstructorDetail.java
@@ -29,6 +29,11 @@ public class InstructorDetail {
     @Column(name="hobby")
     private String hobby;
 
+    // add @OnetoOne annotation
+
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    private Instructor instructor;
+
     public InstructorDetail() {
 
     }
@@ -60,6 +65,14 @@ public class InstructorDetail {
 
     public void setHobby(String hobby) {
         this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
     }
 
     @Override
