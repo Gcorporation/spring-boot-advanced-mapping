29cc8b86e54b819ee3848a791933b450ccedf41f
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-22 11:58:01 +0530

    one to many : JOIN FETCH executing the query

--- src/main/java/com/gcorporation/cruddemoAdvancedMapping/CruddemoAdvancedMappingApplication.java
+++ src/main/java/com/gcorporation/cruddemoAdvancedMapping/CruddemoAdvancedMappingApplication.java
@@ -30,8 +30,22 @@ public class CruddemoAdvancedMappingApplication {
 			//deleteInstructorDetail(appDAO);
 			//createInstructorWithCourses(appDAO);
 			//findInstructorWithCourses(appDAO);
-			findCoursesForInstructor(appDAO);
+			//findCoursesForInstructor(appDAO);
+			findInstructorWithCoursesJoinFetch(appDAO);
 		};
+	}
+
+	private void findInstructorWithCoursesJoinFetch(AppDAO appDAO) {
+		int theId = 1;
+
+		// find the instructor
+		System.out.println("Finding instructor id: " + theId);
+		Instructor tempInstructor = appDAO.findInstructorByIdJoinFetch(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+		System.out.println("Done");
 	}
 
 	private void findCoursesForInstructor(AppDAO appDAO) {
