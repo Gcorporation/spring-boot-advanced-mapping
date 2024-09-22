2bf7759929531fccda0b2a208094761711ca020b
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-22 11:14:17 +0530

    one to many : fetch types - Eager vs Lazy

--- src/main/java/com/gcorporation/cruddemoAdvancedMapping/CruddemoAdvancedMappingApplication.java
+++ src/main/java/com/gcorporation/cruddemoAdvancedMapping/CruddemoAdvancedMappingApplication.java
@@ -27,8 +27,22 @@ public class CruddemoAdvancedMappingApplication {
 			//deleteInstructor(appDAO);
 			//findInstructorDetail(appDAO);
 			//deleteInstructorDetail(appDAO);
-			createInstructorWithCourses(appDAO);
+			//createInstructorWithCourses(appDAO);
+			findInstructorWithCourses(appDAO);
 		};
+	}
+
+	private void findInstructorWithCourses(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findinstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+		System.out.println("Done!");
 	}
 
 	private void createInstructorWithCourses(AppDAO appDAO) {
